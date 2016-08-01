fdae2b875fb393dd2a6528315794fe2e5bce4964
Dag Heyman <dag@yubico.com>
2016-08-01 10:39:50 +0200

    Pep8ify.

--- rovarsprak.py
+++ rovarsprak.py
@@ -1,23 +1,16 @@
-#!/usr/bin/env python3
-
 import fileinput
 
-CONSONANTS = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z']
+CONSONANTS = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
+              'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
+
 
 def parse(text):
-    return ''.join(_parse_line(line) for line in text)
+    return ''.join(_parse_char(char) for line in text for char in line)
 
-def _parse_line(line):
-    return ''.join(_parse_letter(letter) for letter in line)
 
-def _parse_letter(letter):
-    return _add_o(letter) if _consonant(letter) else letter
-
-def _add_o(letter):
-    return letter + "o" + letter.lower()
-
-def _consonant(letter):
-    return letter.lower() in CONSONANTS
+def _parse_char(char):
+    return char + 'o' + char.lower() \
+            if char.lower() in CONSONANTS else char
 
 if __name__ == "__main__":
     print(parse(fileinput.input()))
